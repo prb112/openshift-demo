50761ddf11b5139075a44c3a793f2661737a9299
Paul Bastide <pbastide@us.ibm.com>
2023-07-21 09:53:44 -0400

    fix(sock_shop): delay startup when forking mongod
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- sock_shop/automation/files/user-db-init.sh
+++ sock_shop/automation/files/user-db-init.sh
@@ -6,6 +6,10 @@ SCRIPT_DIR=$(dirname "$0")
 
 mongod --fork --logpath /data/db-users/mongodb.log --dbpath /data/db-users/
 
+# Ref: https://stackoverflow.com/questions/26558932/what-is-the-correct-way-to-wait-until-mongodb-is-ready-after-restart
+# delay while starting up
+while ! /usr/bin/mongo --eval "db.version()" > /dev/null 2>&1; do sleep 0.1; done
+
 # Create the Accounts
 mongo localhost:27017/users accounts-create.js
 
