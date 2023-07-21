4db3c29874b5cc5b56d7eafd4e2c41d325140a85
Paul Bastide <pbastide@us.ibm.com>
2023-07-21 09:49:42 -0400

    fix(sock_shop): refactor the user data creation
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- sock_shop/automation/files/user-db-init.sh
+++ sock_shop/automation/files/user-db-init.sh
@@ -4,16 +4,12 @@
 
 SCRIPT_DIR=$(dirname "$0")
 
-mongod --fork --logpath /var/log/mongodb.log --dbpath /data/db/
+mongod --fork --logpath /data/db-users/mongodb.log --dbpath /data/db-users/
 
-# Ref: https://stackoverflow.com/questions/26558932/what-is-the-correct-way-to-wait-until-mongodb-is-ready-after-restart
-while ! /usr/bin/mongo --eval "db.version()" > /dev/null 2>&1
-do
-    sleep 0.1
-done
+# Create the Accounts
+mongo localhost:27017/users accounts-create.js
 
-FILES=$SCRIPT_DIR/*-create.js
-for f in $FILES; do mongo localhost:27017/users $f; done
-
-FILES=$SCRIPT_DIR/*-insert.js
-for f in $FILES; do mongo localhost:27017/users $f; done
+# Insert useful data
+mongo localhost:27017/users address-insert.js
+mongo localhost:27017/users card-insert.js
+mongo localhost:27017/users customer-insert.js
